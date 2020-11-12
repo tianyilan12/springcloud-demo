7384c84303acbd2fa79aaa90dfb8c57cef0f497e
tianyilan12 <66726288+tianyilan12@users.noreply.github.com>
2020-11-12 15:07:32 +0800

    Update InterventionApplicationWithRibbonConfig.java

--- spring-health-original/intervention-service/src/main/java/com/springhealth/intervention/config/InterventionApplicationWithRibbonConfig.java
+++ spring-health-original/intervention-service/src/main/java/com/springhealth/intervention/config/InterventionApplicationWithRibbonConfig.java
@@ -1,5 +1,4 @@
 package com.springhealth.intervention.config;
-//package com.tianyalan.orders.config;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.SpringApplication;
