bd7c85f7f93f7d4ccd70c43dcc6fe28034b1d912
tianyilan12 <66726288+tianyilan12@users.noreply.github.com>
2020-11-25 17:18:59 +0800

    Update SpringHealthLoadBalanceConfig.java

--- spring-health-stream/intervention-service/src/main/java/com/springhealth/intervention/config/SpringHealthLoadBalanceConfig.java
+++ spring-health-stream/intervention-service/src/main/java/com/springhealth/intervention/config/SpringHealthLoadBalanceConfig.java
@@ -1,5 +1,4 @@
 package com.springhealth.intervention.config;
-//package com.tianyalan.orders.config;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -13,13 +12,10 @@ package com.springhealth.intervention.config;
 //@Configuration
 //public class SpringHealthLoadBalanceConfig {
 //
-//    @Autowired
-//    IClientConfig config;
 //
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public IRule ribbonRule(IClientConfig config) {
-//    	
-//        return new RandomRule();
-//    }
+//  @Bean
+//  public IRule ribbonRule() {
+//  	
+//      return new RandomRule();
+//  }
 //}
